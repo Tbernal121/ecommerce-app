9b37d1fd76807714ac6d13096ad88b98ad967b65
tbernal121 <tomasbernalzuluaga@gmail.com>
2024-05-20 18:34:49 -0500

    ✨ Create Product entity

---
+++ backend/src/entities/product.entity.ts
@@ -0,0 +1,20 @@
+export class Product{   
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    brand: string;
+    image: string; // an url of the image
+    category: string;
+    tags?: string[];
+    discount?: number;
+    rating: number;
+    reviews: string[];
+    weight?: number;
+    height?: number;
+    width?: number;
+    depth?: number;
+    manufacturer?: string;
+    dateAdded: Date;
+}
