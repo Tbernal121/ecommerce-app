1f6dfd74edc75118aa364210f90087108911d806
tbernal121 <tomasbernalzuluaga@gmail.com>
2024-06-05 23:22:57 -0500

    👔 Improve categories controller logic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesController } from './controller/categories.controller';
-import { ProductsController } from './controller/products.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { CategoriesService } from './services/categories.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CategoriesController, ProductsController],
-  providers: [AppService, ProductsService],
+  providers: [AppService, ProductsService, CategoriesService],
 })
 export class AppModule {}
--- backend/src/controllers/categories.controller.ts
+++ backend/src/controllers/categories.controller.ts
@@ -1,28 +1,30 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-
-interface CategoryInfo { // outside the controller class
-    categoryId: string;
-    productId: string;
-  }
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Query, ValidationPipe, UsePipes } from '@nestjs/common';
+import { CategoriesService } from '../services/categories.service';
+import { PositiveIntegerPipe } from '../pipes/positive-integer.pipe';
 
 @Controller('categories')
 export class CategoriesController {
-    @Get(':categoryId') // here no longer 'categories' in @Get('categories/:categoryId') because the @Controller('categoryId') already is giving it to the route
-    getOne(@Param() {categoryId}){ // other option: @Param('categoryId') categoryId: string
-        return  {
-            message: `product ${categoryId}`,
-        };
+    constructor(private categoriesService: CategoriesService) {}
+
+    @Get()
+    getAll() {
+        return this.categoriesService.findAll();
+    }
+
+    @Get(':id')
+    getOne(@Param('id', ParseIntPipe, PositiveIntegerPipe) id: number){
+        return this.categoriesService.findOne(id);
     }
 
     @Get(':categoryId/products/:productId')
-    getOneComplete(@Param('productId') productId: string, @Param('categoryId') categoryId: string){ // other option: @Param() { categoryId, productId }: CategoryInfo
+    getOneComplete(@Param('productId') productId: string, @Param('categoryId') categoryId: string){
         return {
             message: `Product ${productId}, Category ${categoryId}`,
         };
     }
 
     @Post()
-    create(@Body() payload: any){ // other option: @Body() payload: CategoryInfo
+    create(@Body() payload: any){
         return {
             message: 'category created',
             payload,
