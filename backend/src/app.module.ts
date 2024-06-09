f9b31f5d5274dd318fbf93ba130054834281c836
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-06-09 12:26:30 -0500

    :sparkles: Add users module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
