b332709e61b72c30a57af02b5432a8cd542b521a
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-06-15 01:11:15 -0500

    :wrench: Fix modules structure

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsController } from './controllers/products.controller';
+import { BrandsController } from './controllers/brands.controller';
 import { CategoriesService } from './services/categories.service';
 import { ProductsService } from './services/products.service';
+import { BrandsService } from './services/brands.service';
 
 @Module({
-  controllers: [CategoriesController, ProductsController],
-  providers: [CategoriesService, ProductsService],
+  controllers: [CategoriesController, ProductsController, BrandsController],
+  providers: [CategoriesService, ProductsService, BrandsService],
   exports: [ProductsService],
 })
 export class ProductsModule {}
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { UsersService } from './services/users.service';
+import { CustomersService } from './services/customers.service';
 import { UsersController } from './controllers/users.controller';
+import { CustomerController } from './controllers/customers.controller';
 import { ProductsModule } from './../products/products.module';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, CustomerController],
+  providers: [UsersService, CustomersService],
 })
 export class UsersModule {}
