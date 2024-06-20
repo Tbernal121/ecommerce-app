cbbdf28cb817b105bc7684e04fdf1bf541a4ae78
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-06-19 23:49:17 -0500

    :wrench: Add config module for environment variables (including global configuration)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { ControllersController } from './src/products/controllers.controller';
-import { HolasModule } from './holas/holas.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule, HolasModule],
-  controllers: [AppController, ControllersController],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ProductsModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
