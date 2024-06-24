29d053745d7615d4ad96cb5c5bbf958ec323f7b1
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-06-23 23:18:38 -0500

    :rocket: Add deployment configuration for Heroku

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
@@ -12,15 +13,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const configService = app.get(ConfigService);
+
   const config = new DocumentBuilder()
     .setTitle('Ecommerce API')
     .setDescription('This is the documentation for the Ecommerce API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('docs', app, document);
-
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(configService.get<string>('PORT') || 3000);
 }
 bootstrap();
