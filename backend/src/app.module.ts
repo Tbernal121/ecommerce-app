814bf1131eab7fca2393ba9f01d4e20123c851f5
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-01 10:30:47 -0500

    :wrench: Add environment variables configuration and validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
+import config from './common/config';
+import validationSchema from './common/validation-schema';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema,
+    }),
     ProductsModule,
     UsersModule,
   ],
---
+++ backend/src/common/config.ts
@@ -0,0 +1,13 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('config', () => {
+  return {
+    database: {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      name: process.env.POSTGRES_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+  };
+});
