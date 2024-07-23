d12abc5c86bd8cf0c89361a6c98787aacfccb61a
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-22 22:28:08 -0500

    :card_file_box: Add Category Entity columns

--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,11 +1,43 @@
-import { Product } from './product.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+} from 'typeorm';
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  IsUrl,
+  IsUUID,
+} from 'class-validator';
 
-export class Category {
-  id: number;
+@Entity()
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
+  id: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @Column({ type: 'text', nullable: true })
+  @IsOptional()
+  @IsString()
   description?: string;
-  image?: string; // an url of the image
-  parentCategory?: Category; // reference to a parent category (optional)
-  subCategories?: Category[]; // array of subcategories (optional)
-  products?: Product[]; // array of associated products (optional)
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  @IsOptional()
+  @IsUrl()
+  image?: string; // URL of the image
+
+  @CreateDateColumn({ type: 'timestamp' })
+  dateAdded: Date;
+
+  // parentCategory?: Category; // reference to a parent category (optional)
+  // subCategories?: Category[]; // array of subcategories (optional)
+  // products?: Product[]; // array of associated products (optional)
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,12 +6,13 @@ import { ProductsService } from './services/products.service';
 import { Product } from './entities/product.entity';
 import { CategoriesController } from './controllers/categories.controller';
 import { CategoriesService } from './services/categories.service';
+import { Category } from './entities/category.entity';
 import { BrandsController } from './controllers/brands.controller';
 import { BrandsService } from './services/brands.service';
 import { Brand } from './entities/brand.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Brand])],
+  imports: [TypeOrmModule.forFeature([Brand, Category, Product])],
   controllers: [CategoriesController, ProductsController, BrandsController],
   providers: [CategoriesService, ProductsService, BrandsService],
   exports: [ProductsService],
