f69a8f286c4b82d9a35f07b80318ad06bd1d0037
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-22 22:36:24 -0500

    :card_file_box: Add Customer Entity columns

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,7 +1,44 @@
-export class Customer {
-  id: number;
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+} from 'typeorm';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsPhoneNumber,
+  IsUUID,
+} from 'class-validator';
+
+@Entity()
+export class Customer extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
+  id: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  @IsNotEmpty()
+  @IsString()
   lastName: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: false })
+  @IsNotEmpty()
+  @IsPhoneNumber(null) // Uses a default phone number format, can be customized
   phone: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true, nullable: false })
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  dateAdded: Date;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './services/users.service';
-import { CustomersService } from './services/customers.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
 import { CustomerController } from './controllers/customers.controller';
+import { CustomersService } from './services/customers.service';
+import { Customer } from './entities/customer.entity';
 import { ProductsModule } from './../products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forFeature([Customer])],
   controllers: [UsersController, CustomerController],
   providers: [UsersService, CustomersService],
 })
