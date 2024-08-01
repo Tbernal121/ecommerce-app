04a105e34da1d4f6f7c0e5e624ee50e5850a5308
Tomás Bernal Zuluaga <tbernalz@eafit.edu.co>
2024-07-31 23:47:08 -0500

    :fire: Delete all empty .spec files

--- src/common/pipes/positive-integer.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PositiveIntegerPipe } from './positive-integer.pipe';
-
-describe('PositiveIntegerPipe', () => {
-  it('should be defined', () => {
-    expect(new PositiveIntegerPipe()).toBeDefined();
-  });
-});
